9490a6143da474fb449c57d53d59fa6c26e52e0b
lechub <lechub@tlen.pl>
2018-12-27 07:57:09 +0100

    aa

--- Debug/lbutils/FrameBuffer.d
+++
@@ -1,8 +0,0 @@
-lbutils/FrameBuffer.o: ../lbutils/FrameBuffer.cpp \
- ../lbutils/FrameBuffer.h ../lbutils/Fifo.h ../lbutils/myUtils.h
-
-../lbutils/FrameBuffer.h:
-
-../lbutils/Fifo.h:
-
-../lbutils/myUtils.h:
--- Release/src/SilnikNapedu.d
+++
@@ -1,3 +0,0 @@
-src/SilnikNapedu.o: ../src/SilnikNapedu.cpp ../src/SilnikNapedu.h
-
-../src/SilnikNapedu.h:
